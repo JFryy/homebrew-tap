795d6ca47fff09c0d9a6d5620cd31b12dcf84a68
JFryy <fotherby1@gmail.com>
2024-07-30 17:43:20 -0700

    update

--- Formula/qq.rb
+++ Formula/qq.rb
@@ -1,27 +1,27 @@
 class Qq < Formula
   desc "Multi-tool structured format processor for query and transcoding"
   homepage "https://github.com/jfryy/qq"
-  url "https://github.com/jfryy/qq/archive/refs/tags/v0.2.2.tar.gz"
-  sha256 "08b08072b9744103af7ec1447a8976464f69faaf04715f31b3efd4cb8da3adc5"
+  url "https://github.com/jfryy/qq/archive/refs/tags/v0.2.1-re.tar.gz"
+  sha256 "8b568ee47c091a1e70693b0e9ca5175bbc2edf5f5359a173a40d6d1b7be0b957"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/JFryy/qq/releases/download/v0.2.2/qq-v0.2.2-darwin-amd64.tar.gz"
-      sha256 "0558946c788a67eab90a71cd25d12102a6018e6d76eea4f8f56d5cc48f86f61d"
+      url "https://github.com/JFryy/qq/releases/download/v0.2.1-re/qq-v0.2.1-re-darwin-amd64.tar.gz"
+      sha256 "f7403919068014736ae36e19092668409a6b84ae1cf817f0fa424807e4a2e2bc"
     elsif Hardware::CPU.arm?
-      url "https://github.com/JFryy/qq/releases/download/v0.2.2/qq-v0.2.2-darwin-arm64.tar.gz"
-      sha256 "c1b6f7feb8a60e0ab0dcf7b18624b9323936055675df319c533f1e38a5c8c64c"
+      url "https://github.com/JFryy/qq/releases/download/v0.2.1-re/qq-v0.2.1-re-darwin-arm64.tar.gz"
+      sha256 "c015c196032865559eea47d7346a142924eefde0c6599924ab03f53e105d2350"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/JFryy/qq/releases/download/v0.2.2/qq-v0.2.2-linux-amd64.tar.gz"
-      sha256 "776b1e38aab2792cb7824b8cd77299f484481f28b64d7f15da86cb49f7a2a9e2"
+      url "https://github.com/JFryy/qq/releases/download/v0.2.1-re/qq-v0.2.1-re-linux-amd64.tar.gz"
+      sha256 "1aa7a1ee4712f7142b9202babf72ebb5c5216c1f484dd9c83f1174a05c5a3d10"
     elsif Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/JFryy/qq/releases/download/v0.2.2/qq-v0.2.2-linux-arm64.tar.gz"
-      sha256 "0d6ba30c27c477e4954bf8d24865268dc65c07d2da3f6dd5cee862f83383da2b"
+      url "https://github.com/JFryy/qq/releases/download/v0.2.1-re/qq-v0.2.1-re-linux-arm64.tar.gz"
+      sha256 "c912bacc62be17529fcfa0f0d667b99f2ab0a191e9794acedc7b3c6ebbfe4fb1"
     end
   end
 
--- update_formula.sh
+++ update_formula.sh
@@ -2,7 +2,7 @@
 
 # Constants
 REPO="jfryy/qq"
-VERSION="v0.2.2"
+VERSION="v0.2.1-re"
 
 # URLs
 URL_SOURCE="https://github.com/$REPO/archive/refs/tags/$VERSION.tar.gz"
