84c0eb01a731114ee020342bb65d8db046515b2b
James Fotherby <jfotherby@quantcast.com>
2025-01-16 13:32:06 -0800

    iterate version for release

--- Formula/qq.rb
+++ Formula/qq.rb
@@ -1,27 +1,27 @@
 class Qq < Formula
   desc "Multi-tool structured format processor for query and transcoding"
   homepage "https://github.com/jfryy/qq"
-  url "https://github.com/jfryy/qq/archive/refs/tags/v0.2.3.tar.gz"
-  sha256 "cec2d9471d708f566109225a5338960d32a41da29afbcba66d13165b6b968e33"
+  url "https://github.com/jfryy/qq/archive/refs/tags/v0.2.4.tar.gz"
+  sha256 "cdcc643f4ffbda46a9092413193eebfda4cd2894642749ae1be3e9b009c20889"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/JFryy/qq/releases/download/v0.2.3/qq-v0.2.3-darwin-amd64.tar.gz"
-      sha256 "5129ff69c11ac0bd5b13e2d6f5a83c14641c7d3c53fb5478b069c999f5e9006d"
+      url "https://github.com/JFryy/qq/releases/download/v0.2.4/qq-v0.2.4-darwin-amd64.tar.gz"
+      sha256 "cd6448f0511b1d91d0b48f1ac9ce703e769faa0f839b6ab0539b6aafb33ab89c"
     elsif Hardware::CPU.arm?
-      url "https://github.com/JFryy/qq/releases/download/v0.2.3/qq-v0.2.3-darwin-arm64.tar.gz"
-      sha256 "4a4e773d7d5e20ba09f34272b5957f440725fccc77456aa63543b44cf0b43c9d"
+      url "https://github.com/JFryy/qq/releases/download/v0.2.4/qq-v0.2.4-darwin-arm64.tar.gz"
+      sha256 "b78ba7bfda3a9e826e9bd651b0d953bd148d16246743d0576b3ddcd3b9cf42bb"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/JFryy/qq/releases/download/v0.2.3/qq-v0.2.3-linux-amd64.tar.gz"
-      sha256 "21698c1f6010ad7cd1d0b60424cf7de258243773975fb6acbbb0d27d199c6da1"
+      url "https://github.com/JFryy/qq/releases/download/v0.2.4/qq-v0.2.4-linux-amd64.tar.gz"
+      sha256 "162287b4e8c6ab208f0234efdb0f2e95909d8955050b2847ced6d1114db45ead"
     elsif Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/JFryy/qq/releases/download/v0.2.3/qq-v0.2.3-linux-arm64.tar.gz"
-      sha256 "0816667e77c1a551c6c08d116ea13d906dda85a9690697eab3aeef92f0dc5641"
+      url "https://github.com/JFryy/qq/releases/download/v0.2.4/qq-v0.2.4-linux-arm64.tar.gz"
+      sha256 "1e99f6cbe145e72fb01c9263e98a65aaadce3c87fb56bb349a861cc48102568c"
     end
   end
 
