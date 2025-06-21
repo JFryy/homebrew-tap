0eb64c1a783b7e54b963c269dd594c8769610b3c
jfryy <fotherby1@gmail.com>
2025-06-21 13:44:08 -0700

    update qq 0.3.0

--- Formula/qq.rb
+++ Formula/qq.rb
@@ -2,26 +2,26 @@ class Qq < Formula
   desc "Multi-tool structured format processor for query and transcoding"
   homepage "https://github.com/jfryy/qq"
   url "https://github.com/jfryy/qq/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "e6bcc24996f7024f11cf53dd28558cb06d24dc7ed65a7bedb667dd26ab150e90"
+  sha256 "5c8a2ed6c93b79ae43d654cb4d7b2f4ca70db66899d6adecf74cd6ea83ba1601"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/JFryy/qq/releases/download/v0.3.0/qq-v0.3.0-darwin-amd64.tar.gz"
-      sha256 "7f3f5a28c9b50a954082d215ef685c2d05424b844b621f6b36e94fa47bbcd2db"
+      sha256 "31741bb5608bae8b4431a0b820c522c404f2a3771ee416db7c7c008aea59339c"
     elsif Hardware::CPU.arm?
       url "https://github.com/JFryy/qq/releases/download/v0.3.0/qq-v0.3.0-darwin-arm64.tar.gz"
-      sha256 "b328f78ee6dc16124757e00ede86f2634a7fed3cc446c44e241a45441fa3a55a"
+      sha256 "53d877a075d6b9904792a8c34261b92885ad7957f39299d84f1a1dc5eb3b6d13"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/JFryy/qq/releases/download/v0.3.0/qq-v0.3.0-linux-amd64.tar.gz"
-      sha256 "1011276f141cc6cad2e374067c47f970285db71b2f16be7dc80c65975ff17dd3"
+      sha256 "e7f56420ba8b9032f3d2466d39b8fce2368a405432324813813d0cfdeac979da"
     elsif Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/JFryy/qq/releases/download/v0.3.0/qq-v0.3.0-linux-arm64.tar.gz"
-      sha256 "0a85e504f316cc7d03002497120bf7192bb5bc92b7a0402335be9d54add5e80b"
+      sha256 "45e0d892396f289e0c9fc921202e00af9d68a71a2075e8cacc3d64decc6eac65"
     end
   end
 
